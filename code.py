4f154615ee68554dcb40f0f5fc5cabfce81aa031
gmate1 <mate.gomori@gmail.com>
2024-05-09 15:44:49 +0200

    2. feladat

--- code.py
+++ code.py
@@ -6,4 +6,19 @@ class Szoba(ABC):
     def __init__(self, ar, szobaszam):
         self.ar = ar
         self.szobaszam = szobaszam
+
+    pass
+
+
+class EgyAgyasSzoba(Szoba):
+    def __init__(self, szobaszam):
+        super().__init__(50, szobaszam)
+
+    pass
+
+
+class KetAgyasSzoba(Szoba):
+    def __init__(self, szobaszam):
+        super().__init__(100, szobaszam)
+
     pass
