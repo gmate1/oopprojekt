133fc03e5551bcfdffeab54e3acbfcd2cb50ebc5
gmate1 <mate.gomori@gmail.com>
2024-05-09 16:30:56 +0200

    5. feladat

--- code.py
+++ code.py
@@ -3,10 +3,9 @@ from abc import ABC
 
 class Szoba(ABC):
 
-    def __init__(self, ar, szobaszam, foglalt=False):
+    def __init__(self, ar, szobaszam):
         self.ar = ar
         self.szobaszam = szobaszam
-        self.foglalt = foglalt
 
     pass
 
@@ -31,14 +30,25 @@ class Szalloda:
         self.szobak = szobak
         self.foglalasok = []
 
+    def foglalas(self, szobaszam, datum, nev):
+        talalt_szoba = next((szoba for szoba in self.szobak if szoba.szobaszam == szobaszam), None)
+        if talalt_szoba is None:
+            return "Nincs ilyen szobaszámú szoba a szállodában"
+        else:
+            foglalas = Foglalas(talalt_szoba, datum, nev)
+            self.foglalasok.append(foglalas)
+            return foglalas.ar
+
     pass
 
 
 class Foglalas:
 
-    def __init__(self, szobaszam, datum):
-        self.szobaszam = szobaszam
+    def __init__(self, szoba, datum, nev):
+        self.szoba = szoba
         self.datum = datum
         self.nap = 1
+        self.nev = nev
+        self.ar = self.nap * szoba.ar
 
     pass
